77658d4072c14a0655b0a874270dd9ba0569875a
chenxiao <ccwinner@ccwinnerdeMacBook-Pro.local>
2016-11-11 14:28:00 +0800

    FIX:cannot set node theme

--- src/RNGestureView.ios.js
+++ src/RNGestureView.ios.js
@@ -1,21 +1,33 @@
 import React, { Component, PropTypes } from 'react';
-import { requireNativeComponent } from 'react-native';
+import { requireNativeComponent, NativeModules } from 'react-native';
 
+const GestureManager = NativeModules.RNGestureViewManager
 const NativeGestureView = requireNativeComponent('RNGestureView', RNGestureView);
 export default class RNGestureView extends Component {
 
     _onGestureComplete(event) {
-        if (this.props.onGestureComplete == null) {
+        if (!this.props.onGestureComplete) {
             return;
         }
         this.props.onGestureComplete(event.nativeEvent.completion)
+    }
+
+    componentDidMount() {
+        if (this.props.nodeThemes) {
+            GestureManager.setupNodeUI(this.props.nodeThemes);
+        }
     }
 
     static propTypes = {
         nodeScale: PropTypes.number,
         colCount: PropTypes.number,
         onGestureComplete:PropTypes.func,
-        backgroundImgName:PropTypes.string
+        backgroundImgName:PropTypes.string,
+        nodeThemes: PropTypes.shape({
+            nodeNormal: PropTypes.string,
+            nodeError: PropTypes.string,
+            nodeSelect: PropTypes.string
+        })
     }
 
     render() {
